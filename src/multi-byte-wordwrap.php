97762eec2d7b258bc791e050b39d2bc15f360bff
Joe Tannenbaum <joe@joe.codes>
2024-01-02 15:06:03 -0800

    initial commit

---
+++ src/multi-byte-wordwrap.php
@@ -0,0 +1,77 @@
+<?php
+
+declare(strict_types=1);
+
+function mb_wordwrap(
+    string $string,
+    int $width = 75,
+    string $break = "\n",
+    bool $cut_long_words = false
+): string {
+    $words = explode(' ', $string);
+    $result = '';
+    $line = null;
+    $lineWidth = 0;
+
+    if ($cut_long_words) {
+        foreach ($words as $index => $word) {
+            $characters = mb_str_split($word);
+            $strings = [];
+            $string = '';
+
+            foreach ($characters as $character) {
+                $tmp = $string . $character;
+
+                if (mb_strwidth($tmp) > $width) {
+                    $strings[] = $string;
+                    $string = $character;
+                } else {
+                    $string = $tmp;
+                }
+            }
+
+            if ($string !== '') {
+                $strings[] = $string;
+            }
+
+            $words[$index] = implode(' ', $strings);
+        }
+
+        $words = explode(' ', implode(' ', $words));
+    }
+
+    foreach ($words as $word) {
+        $tmp = ($line === null) ? $word : $line . ' ' . $word;
+
+        // Look for zero-width joiner characters (combined emojis)
+        preg_match('/\p{Cf}/u', $word, $joinerMatches);
+
+        $wordWidth = count($joinerMatches) > 0 ? 2 : mb_strwidth($word);
+
+        $lineWidth += $wordWidth;
+
+        if ($line !== null) {
+            // Space between words
+            $lineWidth += 1;
+        }
+
+        if ($lineWidth <= $width) {
+            $line = $tmp;
+        } else {
+            if ($line !== '') {
+                $result .= $line . $break;
+            }
+
+            $line = $word;
+            $lineWidth = $wordWidth;
+        }
+    }
+
+    if ($line !== '') {
+        $result .= $line;
+    }
+
+    $result = $result;
+
+    return $result;
+}
